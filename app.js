f47be251c14ff11d994cc2e68e352969d592fa51
Sampson <dujian@tuya.com>
2022-03-07 21:35:20 +0800

    1

---
+++ app.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+const express = require('express')
+
+const app = express()
+
+const port = 3001
+
+
+/**
+ * @type {puppeteer.Browser}
+ */
+let browser
+
+(async () => {
+  browser = await puppeteer.launch({});
+})();
+
+  /**
+   * @type {Array<puppeteer.Page>}
+   */
+  const pageList = []
+
+  app.get('/newpage', async (req, res, next) => {
+    if (!browser) {
+      return res.send(404)
+    }
+    try {
+      console.log('当前第', pageList.length, '个')
+
+      const page = await browser.newPage();
+      await page.goto(req.query.url || 'https://live.douyin.com/770596782915');
+      pageList.push(page)
+      res.sendStatus(200)
+
+    } catch (error) {
+      res.sendStatus(500)
+    }
+  })
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
